9a1d6e51c378428bfa465e9d0997d01fdd34176a
e <zhu923362049@163.com>
2020-08-30 18:52:46 +0800

    second

--- baidu-shop-parent/baidu-shop-basics/baidu-shop-basics-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
+++ baidu-shop-parent/baidu-shop-basics/baidu-shop-basics-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
@@ -17,7 +17,6 @@ public class RunEurekaServerApplication {
 
 
     public static void main(String[] args) {
-        System.out.println("第一个项目，第二遍");
         SpringApplication.run(RunEurekaServerApplication.class);
     }
 
